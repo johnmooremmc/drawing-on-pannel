b0ea2cab34b80177ee8e3b7de0ddf8d7fbf0e565
John Moore <johnmoore@mmc.school.nz>
2021-05-26 10:59:25 +1200

    Add project files.

---
+++ drawing/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlDraw = new System.Windows.Forms.Panel();
+            this.btnExe = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.tmrTick = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlDraw
+            // 
+            this.pnlDraw.Location = new System.Drawing.Point(26, 25);
+            this.pnlDraw.Name = "pnlDraw";
+            this.pnlDraw.Size = new System.Drawing.Size(661, 265);
+            this.pnlDraw.TabIndex = 0;
+            this.pnlDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseMove);
+            // 
+            // btnExe
+            // 
+            this.btnExe.Location = new System.Drawing.Point(26, 321);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(131, 80);
+            this.btnExe.TabIndex = 1;
+            this.btnExe.Text = "Hide Drawing";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(163, 321);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(128, 80);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // tmrTick
+            // 
+            this.tmrTick.Enabled = true;
+            this.tmrTick.Interval = 10;
+            this.tmrTick.Tick += new System.EventHandler(this.tmrTick_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.pnlDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDraw;
+        private System.Windows.Forms.Button btnExe;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Timer tmrTick;
+    }
+}
+
---
+++ drawing/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        bool triggerDraw = false;
+
+        int x;
+        int y;
+
+
+       
+        public void ExeShowHide()
+        {
+            pnlDraw.Refresh();
+
+        }
+
+        private void btnExe_Click(object sender, EventArgs e)
+        {
+            ExeShowHide();
+        }
+
+        private void pnlDraw_Paint()
+        {
+            
+                Graphics paper = pnlDraw.CreateGraphics();
+
+                Pen blackPen = new Pen(Color.Black, 3); Font fy = new Font("Helvetica", 10, FontStyle.Bold);
+                Brush br = new SolidBrush(System.Drawing.Color.Red);
+
+
+
+                Rectangle rect = new Rectangle(x, y, 50, 50);
+                paper.DrawRectangle(blackPen, rect);
+                paper.FillRectangle(br, rect);
+
+            
+
+        }
+
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrTick_Tick(object sender, EventArgs e)
+        {
+
+           
+            
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void pnlDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            pnlDraw.Refresh();
+            pnlDraw_Paint();
+        }
+    }
+}
